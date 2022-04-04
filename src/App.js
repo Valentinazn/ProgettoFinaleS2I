e9825b426cb599b2cf23fde4aacb317ca676ea8c
Valentinazn <85753606+Valentinazn@users.noreply.github.com>
2022-04-04 16:52:12 +0200

    commit change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom';
-import React, { useState } from 'react'
+import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Cart from './components/Cart';
